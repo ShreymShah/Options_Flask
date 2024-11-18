3a8d9d9ff9bb1b5174d29f8323d5c79ec9ed21f1
ShreymShah <93068854+ShreymShah@users.noreply.github.com>
2024-11-17 22:32:19 -0800

    Update app.py

--- app.py
+++ app.py
@@ -235,13 +235,13 @@ def shift(username, api_key, previous_call_sold, new_call_sell,previous_call_hed
             PlaceBuyOrder(alice, 1800, False, new_put_hedge, current_expiry_hedge)
 
         if int(previous_call_hedge) != 1 and int(new_call_hedge) != 1:
-            PlaceBuyOrder(alice, 1800, False, new_call_hedge, current_expiry_hedge)
+            PlaceBuyOrder(alice, 1800, True, new_call_hedge, current_expiry_hedge)
             
     if int(previous_put_hedge) != 1 and int(new_put_hedge) != 1:
         PlaceBuyOrder(alice, qty - (1800*a), False, new_put_hedge, current_expiry_hedge)
 
     if int(previous_call_hedge) != 1 and int(new_call_hedge) != 1:
-        PlaceBuyOrder(alice, qty - (1800*a), False, new_call_hedge, current_expiry_hedge)
+        PlaceBuyOrder(alice, qty - (1800*a), True, new_call_hedge, current_expiry_hedge)
 
     #squareoff currently sold put
     
@@ -251,25 +251,25 @@ def shift(username, api_key, previous_call_sold, new_call_sell,previous_call_hed
             PlaceBuyOrder(alice,1800,False,previous_put_sold,previous_expiry_sell)
 
         if int(previous_call_sold)!=1 and int(new_call_sell)!=1:
-            PlaceBuyOrder(alice,1800,False,previous_call_sold,previous_expiry_sell)
+            PlaceBuyOrder(alice,1800,True,previous_call_sold,previous_expiry_sell)
             
         if int(previous_put_sold)!=1 and int(new_put_sell)!=1:
             PlaceSellOrder(alice, 1800, False,new_put_sell,current_expiry_sell)
 
         if int(previous_call_sold)!=1 and int(new_call_sell)!=1:
-            PlaceSellOrder(alice, 1800, False,new_call_sell,current_expiry_sell)
+            PlaceSellOrder(alice, 1800, True,new_call_sell,current_expiry_sell)
 
     if int(previous_put_sold)!=1 and int(new_put_sell)!=1:
-        PlaceBuyOrder(alice,1800,False,previous_put_sold,previous_expiry_sell)
+        PlaceBuyOrder(alice,qty - (1800*a),False,previous_put_sold,previous_expiry_sell)
 
     if int(previous_call_sold)!=1 and int(new_call_sell)!=1:
-        PlaceBuyOrder(alice,1800,False,previous_call_sold,previous_expiry_sell)
+        PlaceBuyOrder(alice,qty - (1800*a),True,previous_call_sold,previous_expiry_sell)
             
     if int(previous_put_sold)!=1 and int(new_put_sell)!=1:
-        PlaceSellOrder(alice, 1800, False,new_put_sell,current_expiry_sell)
+        PlaceSellOrder(alice, qty - (1800*a), False,new_put_sell,current_expiry_sell)
 
     if int(previous_call_sold)!=1 and int(new_call_sell)!=1:
-        PlaceSellOrder(alice, 1800, False,new_call_sell,current_expiry_sell)
+        PlaceSellOrder(alice, qty - (1800*a), True,new_call_sell,current_expiry_sell)
 
 
 
@@ -281,13 +281,13 @@ def shift(username, api_key, previous_call_sold, new_call_sell,previous_call_hed
             PlaceSellOrder(alice, 1800, False, previous_put_hedge, previous_expiry_hedge)
 
         if int(previous_call_hedge) != 1 and int(new_call_hedge) != 1:
-            PlaceSellOrder(alice, 1800, False, previous_call_hedge, previous_expiry_hedge)
+            PlaceSellOrder(alice, 1800, True, previous_call_hedge, previous_expiry_hedge)
             
     if int(previous_put_hedge) != 1 and int(new_put_hedge) != 1:
         PlaceSellOrder(alice, qty - (1800*a), False, previous_put_hedge, previous_expiry_hedge)
 
     if int(previous_call_hedge) != 1 and int(new_call_hedge) != 1:
-        PlaceSellOrder(alice, qty - (1800*a), False, previous_call_hedge, previous_expiry_hedge)
+        PlaceSellOrder(alice, qty - (1800*a), True, previous_call_hedge, previous_expiry_hedge)
         
         
 
